fdfa8f04aaeb5afc851c82d0816c627db137f167
Bruno Andreotti <bandreotti@protonmail.com>
2019-08-02 17:39:10 -0300

    Basic camera movement

---
+++ Scripts/Camera.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public class Camera : Node2D
+{
+	const int SPEED = 600;
+	int targetPositionX = 0;
+
+	public override void _Process(float delta)
+	{
+		if (targetPositionX - Position.x > SPEED * delta)
+			Position += new Vector2(SPEED * delta, 0);
+		else
+			Position = new Vector2(targetPositionX, 0);
+
+		if (Input.IsActionJustPressed("test"))
+			this.NextLevel();
+	}
+
+	public void NextLevel()
+	{
+		targetPositionX += 1280;
+	}
+}
