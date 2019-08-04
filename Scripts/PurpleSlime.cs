f54afcedc0e49ac7ef03bdd9aa0a6a222e34c65f
Samuel <samuel@pop-os.localdomain>
2019-08-04 10:13:39 -0300

    commit

--- Scripts/PurpleSlime.cs
+++ Scripts/PurpleSlime.cs
@@ -47,15 +47,19 @@ public class PurpleSlime : BasicSlime
 
 		velocity = (new Vector2(direction, -1) * jumpStrength);
 
-		if (velocity.y >= 0 && shootTimer.TimeLeft > 0.5f) {
+		if (velocity.y >= 0)
 			GetNode<AnimatedSprite>("SlimeSprite").SetAnimation("default");
-			GetNode<AnimatedSprite>("ArmorSprite").SetAnimation("default");
-		} else if (shootTimer.TimeLeft > 0.5f) {
+		else 
 			GetNode<AnimatedSprite>("SlimeSprite").SetAnimation("jump");
-			GetNode<AnimatedSprite>("ArmorSprite").SetAnimation("jump");
-		} else {
+ 
+		
+		if (shootTimer.TimeLeft < 0.2f) {
 			GetNode<AnimatedSprite>("ArmorSprite").SetAnimation("shooting");
 			jumpTimer.Start();
+		} else if (velocity.y >= 0) {
+			GetNode<AnimatedSprite>("ArmorSprite").SetAnimation("default");
+		} else { 
+			GetNode<AnimatedSprite>("ArmorSprite").SetAnimation("jump");
 		}
 
 		
